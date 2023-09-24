3c87ef1fff09f17f14916adb33c3143cb6a96e03
Kartavya Thapa <hennybeerseeker@gmail.com>
2023-09-24 18:55:06 +0530

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,70 @@
+//express in ts
+import express from 'express';
+import { Request, Response } from 'express';
+import cors from 'cors';
+/* import multer from 'multer'; */
+import ytdl from 'ytdl-core';
+import Ffmpeg from 'fluent-ffmpeg';
+import ffmpegPath from 'ffmpeg-static';
+
+Ffmpeg.setFfmpegPath(ffmpegPath as unknown as string);
+/* const storage = multer.diskStorage({
+    filename: function (_req, file, cb) {
+        cb(null, file.originalname)
+    },
+    destination: function (_req, _file, cb) {
+        cb(null, './uploads')
+    }
+})
+
+const upload = multer({storage});
+ */
+const app = express();
+const port = 3000;
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.text());
+app.use(cors());
+app.get('/', (_req: Request, res: Response) => {
+    res.send('Hello World!');
+});
+
+app.post('/',(_req: Request, res: Response) => {
+    if(typeof _req.body === 'string') {
+        try{
+            const handleUrl =async () => {
+                const info = await ytdl.getInfo(_req.body as string);
+                const format = ytdl.chooseFormat(info.formats, 
+                { quality: 'highest',
+                  filter: 'audioonly' 
+                });
+                res.set({ "Content-Type": "audio/mpeg" });
+
+                await new Promise((resolve, reject) => {
+                    const ffmpegCommand = Ffmpeg().input(ytdl.downloadFromInfo(info,{
+                        format: format }))
+                        .audioBitrate(128)
+                        .audioCodec('libmp3lame')
+                        .format('mp3')
+                        .on('end', ()=>console.log('Done converting'))
+                        .on('error', ()=>console.log('Error while converting'))
+                        resolve(ffmpegCommand.pipe(res));
+                        reject('Error while converting' +"\n"+ffmpegCommand);
+                })
+            }
+            
+            handleUrl();
+        }
+        catch(err){
+            console.log(err);
+            res.end('Error in parsing url');
+        }
+    }
+});
+
+app.listen(port, () => {
+    console.log(`server started at http://localhost:${port}`);
+});
+
+
+
